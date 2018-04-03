26416f1884a7ab7e659d69f27cdf7dff56b787f7
killertux <bruno.canguru@hotmail.com>
2018-04-02 21:16:07 -0300

    Added FLAGS

---
+++ LAB1/ProjectFiles/Flags.h
@@ -0,0 +1,13 @@
+#ifndef FLAGS_H
+#define FLAGS_H
+
+bool generated = false;
+bool prime = false;
+bool prime_await = true;
+bool finished_decoding = false;
+bool passed_last = false;
+bool passed_last_await = true;
+bool passed_penultimate = false;
+bool passed_penultimate_await = false;
+
+#endif
--- LAB1/ProjectFiles/KeyGenerator.c
+++ LAB1/ProjectFiles/KeyGenerator.c
@@ -1,8 +1,16 @@
 #include "cmsis_os.h"
 #include "KeyGenerator.h"
+#include "Flags.h"
 
 osThreadId tid_KeyGenerator;                            // thread id
 osThreadDef (key_generator, osPriorityNormal, 1, 0);
+
+int init_key_generator()
+{
+	tid_KeyGenerator = osThreadCreate (osThread(key_generator), NULL);
+  if (!tid_KeyGenerator) return(-1);
+	return 0;
+}
 
 void key_generator()
 {
